54f97448c2e65f632a0c63b2129b24ef601b9944
John Danial <john.danial@me.com>
2024-01-30 23:00:03 -0500

    Initial Commit

---
+++ betaSpin/betaSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  betaSpinApp.swift
+//  betaSpin
+//
+//  Created by johndanial on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct betaSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ betaSpinUITests/betaSpinUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  betaSpinUITestsLaunchTests.swift
+//  betaSpinUITests
+//
+//  Created by johndanial on 1/30/24.
+//
+
+import XCTest
+
+final class betaSpinUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
